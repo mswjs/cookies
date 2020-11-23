b0ba434852b042d98e9c5ef12f8590ce57d7281b
marcosvega91 <marcomoretti0103@gmail.com>
2020-11-23 16:34:21 +0100

    fix: update warning message

--- src/CookieStore.ts
+++ src/CookieStore.ts
@@ -101,20 +101,27 @@ class CookieStore {
     const persistedCookies = localStorage.getItem(PERSISTENCY_KEY)
 
     if (persistedCookies) {
-      try{
+      try {
         const parsedCookies: [string, [string, any]] = JSON.parse(
-          persistedCookies
+          persistedCookies,
         )
-  
+
         parsedCookies.forEach(([origin, cookie]) => {
           this.store.set(origin, new Map(cookie))
         })
-      }catch(_){
-        console.warn(`[MSW] the storage used for cookies has an invalid value "${localStorage.getItem(PERSISTENCY_KEY)}".
-This key is used internally by MSW to store cookies.`)
+      } catch (error) {
+        console.warn(`
+[virtual-cookie] Failed to parse a stored cookie from the localStorage (key "${PERSISTENCY_KEY}").
+
+Stored value:
+${localStorage.getItem(PERSISTENCY_KEY)}
+
+Thrown exception:
+${error}
+
+Invalid value has been removed from localStorage to prevent subsequent failed parsing attempts.`)
         localStorage.removeItem(PERSISTENCY_KEY)
       }
-      
     }
   }
 
@@ -130,7 +137,7 @@ This key is used internally by MSW to store cookies.`)
     const serializedCookies = Array.from(this.store.entries()).map(
       ([origin, cookies]) => {
         return [origin, Array.from(cookies.entries())]
-      }
+      },
     )
 
     localStorage.setItem(PERSISTENCY_KEY, JSON.stringify(serializedCookies))
